12bd488b9823b462107dd2f17f33fb54d3664d65
Chicosk1 <168223635+Chicosk1@users.noreply.github.com>
2024-09-17 08:27:21 -0300

    Update Pessoa.java

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,35 +1,30 @@
-// Class Pessoa para encapsular os dados da pessoa
 class Pessoa {
     private String nomePessoa;
     private int idadePessoa;
     private Endereco enderecoPessoa;
 
-    // Setter para definir o nome da pessoa
+    // Setters
     public void setNomePessoa(String nomePessoa) {
         this.nomePessoa = nomePessoa;
     }
-
-    // Getter para obter o nome da pessoa
-    public String getNomePessoa() {
-        return nomePessoa;
-    }
-
-    // Setter para definir a idade da pessoa
+    
     public void setIdadePessoa(int idadePessoa) {
         this.idadePessoa = idadePessoa;
     }
-
-    // Getter para obter a idade da pessoa
-    public int getIdadePessoa() {
-        return idadePessoa;
-    }
-
-    // Setter para definir o endereço da pessoa
+    
     public void setEnderecoPessoa(Endereco enderecoPessoa) {
         this.enderecoPessoa = enderecoPessoa;
     }
 
-    // Getter para obter o endereço da pessoa
+    // Getters
+    public String getNomePessoa() {
+        return nomePessoa;
+    }
+    
+    public int getIdadePessoa() {
+        return idadePessoa;
+    }
+
     public Endereco getEnderecoPessoa() {
         return enderecoPessoa;
     }
